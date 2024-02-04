d0699fbd8aa962b9c7bf976f796373f6d66b96d5
randa sabra <randasabra27@gmail.com>
2024-02-04 16:40:05 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel {
+  String? title;
+  String? subTitle;
+  String? date;
+  int? color;
+  NoteModel({required this.title,required this.subTitle,required this.date,required this.color});
+
+}
