cb7b671bbce5f9d5f008b1b967e3be048bfc3e6d
randa sabra <randasabra27@gmail.com>
2024-02-04 20:42:59 +0200

    Add notes states

--- lib/cubit/states.dart
+++ lib/cubit/states.dart
@@ -1,3 +1,17 @@
 abstract class NotesStates{}
 
 class  NotesInitial extends NotesStates{}
+
+class  NotesLoading extends NotesStates{}
+
+class  NotesSuccess extends NotesStates{}
+
+class  NotesError extends NotesStates{
+
+  final String? error;
+  NotesError(this.error);
+
+}
+
+
+
